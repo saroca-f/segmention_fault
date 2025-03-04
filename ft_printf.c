2adf6228103e57fd3caffeaeaff1a92cd073d7e1
Matteo Lucien Jeannin Rocco <mjeannin@c2r3s5.42malaga.com>
2025-03-04 16:56:49 +0100

    first push

---
+++ ft_printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+    
+void    ft_printf()
+{
+    printf("Hola Universo!");
+}
--- minishell.c
+++ minishell.c
@@ -4,3 +4,12 @@ int main()
 {
 	return(printf("Hola Mundo!"));
 }
+
+int ft_putstr(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+        write(1, &str[i++], 1);
+    return i;
+}
